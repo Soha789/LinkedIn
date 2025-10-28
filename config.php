893b02515a817cd1b79cfb2cdd1cb25b0e320bc1
Soha <sohasalehzain@gmail.com>
2025-10-28 09:46:36 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "ugfwxemowrehd";
+$pass = "cliigx0v0hca";
+$db = "dbouudzeflm8hv";
+
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
